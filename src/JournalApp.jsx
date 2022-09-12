c1de8b934436f9fdb2a09b8e5ed3e0695ed5c653
Alex Martin <alextravc.342@gmail.com>
2022-09-12 06:56:34 -0500

    [ADD]: init config MUI + Vite - theme/

---
+++ src/JournalApp.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter } from 'react-router-dom';
+
+import { AppRouter } from './router/AppRouter';
+import { AppTheme } from './theme/AppTheme';
+
+export const JournalApp = () => {
+  return (
+    <AppTheme>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </AppTheme>
+  );
+};
